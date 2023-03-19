d6959ea1f267ad99cef4ace978adae289e0e448b
Noor <>
2023-03-19 22:47:38 +0200

    create search screens

--- app/src/main/java/com/example/chickenmasala/SearchResultFragment.kt
+++ app/src/main/java/com/example/chickenmasala/SearchResultFragment.kt
@@ -34,7 +34,7 @@ class SearchResultFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_result, container, false)
+        return inflater.inflate(R.layout.empty_search_result, container, false)
     }
 
     companion object {
