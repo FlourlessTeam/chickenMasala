7b53d02ef511b6df85b71f1536a1ebb12e882121
Noor <>
2023-03-21 14:24:14 +0200

    set up favourite result fragment

---
+++ app/src/main/java/com/example/chickenmasala/FavouriteResultFragment.kt
@@ -0,0 +1,24 @@
+package com.example.chickenmasala
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.example.chickenmasala.databinding.FavouriteResultBinding
+
+class FavouriteResultFragment: Fragment() {
+
+    lateinit var binding: FavouriteResultBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FavouriteResultBinding.inflate(inflater,container,false)
+        val itemsView = layoutInflater.inflate(R.layout.favourite_content, binding.listContainer, false ) as ConstraintLayout
+        return binding.root
+    }
+}
