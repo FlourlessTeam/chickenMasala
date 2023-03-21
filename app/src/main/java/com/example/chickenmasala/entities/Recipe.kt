e8d3215fa0f87b3035f2df18cfafc6bc16c1ca73
Abdelwahab Elsayed <bdmstf@gmail.com>
2023-03-21 17:02:45 +0200

    add interactors

--- app/src/main/java/com/example/chickenmasala/entities/Recipe.kt
+++ app/src/main/java/com/example/chickenmasala/entities/Recipe.kt
@@ -1,13 +1,13 @@
-package com.example.chickenmasala.data.entities
+package com.example.chickenmasala.entities
 
 data class Recipe(
     val translatedRecipeName: String,
-    val translatedIngredients: String,
+    val translatedIngredients: List<String>,
     val totalTimeInMins: Int,
     val cuisine: String,
-    val translatedInstructions: String,
+    val translatedInstructions: List<String>,
     val url: String,
-    val cleanedIngredients: String,
+    val cleanedIngredients: List<String>,
     val imageUrl: String,
     val ingredientCount: Int,
     var isFavourite:Boolean = false
