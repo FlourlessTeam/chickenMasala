c2d26d386d29e0c57e47883244bf919564ceda56
Abdelrahman Mohamed <Abdelrahmanmohamed82@outlook.com>
2023-03-21 22:51:55 +0200

    add cuisine fragment

--- app/src/main/java/com/example/chickenmasala/ui/MainActivity.kt
+++ app/src/main/java/com/example/chickenmasala/ui/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.chickenmasala.ui
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
 import com.example.chickenmasala.R
 import com.example.chickenmasala.data.DataManager
+import com.example.chickenmasala.entities.Recipe
+import com.example.chickenmasala.data.util.CsvParser
+import com.example.chickenmasala.interactors.GetAllCuisines
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     val dataManager by lazy { DataManager(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().add(R.id.nav_host_fragment, SubcategoryFragment())
+        supportFragmentManager.beginTransaction()
+            .add(R.id.nav_host_fragment,SubcategoryFragment())
             .commit()
 
 
     }
+
+
+
 
 
 }
