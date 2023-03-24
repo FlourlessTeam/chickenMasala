8aaf6ded9e02fe11bcd100842dd3d17e66970346
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-03-24 13:41:41 +0300

    try to add some logic on chipgroup

--- app/src/main/java/com/example/chickenmasala/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/chickenmasala/ui/fragments/HomeFragment.kt
@@ -1,27 +1,50 @@
 package com.example.chickenmasala.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import com.example.chickenmasala.data.DataManager
 import com.example.chickenmasala.databinding.FragmentHomeBinding
-import com.example.chickenmasala.interactors.GetCuisineImageAndName
+
+//import android.R
+//import com.google.android.material.chip.Chip
+//import com.google.android.material.chip.ChipDrawable
+//import com.example.chickenmasala.R
+//import com.example.chickenmasala.data.DataManager
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
-
-
-    private val dataManager by lazy { DataManager(requireContext()) }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setupViews()
     }
 
+    /*
     private fun setupViews() {
-//        binding.ContentTextView.text = GetCuisineImageAndName(dataManager).execute().toString()
-    }
+        val chip1 = Chip(context, null, R.style.SingleChip_SelectedChip)
+        chip1.isChecked = true
+        chip1.text = "Indian"
+        // Set the checked and unchecked styles for the Chip
+        chip1.setChipDrawable(
+            ChipDrawable.createFromResource(
+                requireContext(),
+                R.style.SingleChip_SelectedChip
+            )
+        )
+        chip1.setChipDrawable(
+            ChipDrawable.createFromResource(
+                requireContext(),
+                R.style.SingleChip_UnSelectedChip
+            )
+        )
+        val chip2 = Chip(context, null, R.style.SingleChip_UnSelectedChip)
+        chip2.text = "South Indian"
 
+        binding.cuisinesChipGroup.apply {
+
+            addView(chip1)
+            addView(chip2)
+
+
+        }
+    }
+*/
 
 }
 
