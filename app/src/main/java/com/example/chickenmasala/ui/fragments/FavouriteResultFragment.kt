afe36e59e0c8d101e622efe5a1a4efb52cbe9b00
Noor <>
2023-03-24 11:00:34 +0200

    write logic for favourite result fragment

--- app/src/main/java/com/example/chickenmasala/ui/fragments/FavouriteResultFragment.kt
+++ app/src/main/java/com/example/chickenmasala/ui/fragments/FavouriteResultFragment.kt
@@ -1,20 +1,59 @@
 package com.example.chickenmasala.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chickenmasala.data.DataManager
 import com.example.chickenmasala.databinding.FavouriteResultBinding
+import com.example.chickenmasala.entities.Recipe
 import com.example.chickenmasala.interactors.GetFavoritesRecipes
+import com.example.chickenmasala.ui.adapters.RecipesAdapter
 
-class FavouriteResultFragment: BaseFragment<FavouriteResultBinding> (FavouriteResultBinding::inflate){
+class FavouriteResultFragment: BaseFragment<FavouriteResultBinding> (FavouriteResultBinding::inflate) {
+
+    private val dataManger by lazy { DataManager(requireContext()) }
+    private val getFavoritesRecipes by lazy { GetFavoritesRecipes(dataManger) }
+    private val recipesAdapter = RecipesAdapter()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val favouriteResults = getFavoritesRecipes.execute()
+        handleFavouriteResults(favouriteResults)
     }
 
+    private fun handleFavouriteResults(favouriteResults: List<Recipe>) {
+        when {
+            favouriteResults.isNotEmpty() -> showFavouriteResults(favouriteResults)
+            else -> showEmptyState()
+        }
+    }
 
+    private fun showFavouriteResults(favouriteResults: List<Recipe>) {
+        binding.apply {
+            iconBack.isVisible = true
+            textFavourite.isVisible = true
+            imageFavouriteEmpty.isVisible = false
+            textFavouriteEmpty.isVisible = false
+            textEmptyFavouriteDetails.isVisible = false
+            recyclerViewFavourite.isVisible = true
+            recyclerViewFavourite.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+                adapter = recipesAdapter
+            }
+            recipesAdapter.submitList(favouriteResults)
+        }
+    }
 
+    private fun showEmptyState() {
+        binding.apply {
+            iconBack.isVisible = true
+            textFavourite.isVisible = true
+            imageFavouriteEmpty.isVisible = true
+            textFavouriteEmpty.isVisible = true
+            textEmptyFavouriteDetails.isVisible = true
+            recyclerViewFavourite.isVisible = false
+        }
+
+    }
 
 }
