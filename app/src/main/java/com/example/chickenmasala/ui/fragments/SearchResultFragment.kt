419887358ff58d8aaf627a7d3983fc551f02bce0
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-03-24 04:56:35 +0200

    apply recycle view in SearchResultFragment

--- app/src/main/java/com/example/chickenmasala/ui/fragments/SearchResultFragment.kt
+++ app/src/main/java/com/example/chickenmasala/ui/fragments/SearchResultFragment.kt
@@ -2,29 +2,27 @@ package com.example.chickenmasala.ui.fragments
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
 import android.widget.SearchView
-import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.bumptech.glide.Glide
-import com.example.chickenmasala.R
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.chickenmasala.data.DataManager
 import com.example.chickenmasala.databinding.SearchResultBinding
 import com.example.chickenmasala.entities.Recipe
 import com.example.chickenmasala.interactors.SearchRecipes
+import com.example.chickenmasala.ui.adapters.RecipesAdapter
 
 class SearchResultFragment : BaseFragment<SearchResultBinding>(SearchResultBinding::inflate) {
 
-
     private val dataManager by lazy { DataManager(requireContext()) }
     private val searchRecipes by lazy { SearchRecipes(dataManager) }
+    private val recipesAdapter = RecipesAdapter()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         setupSearchView()
     }
 
     private fun setupSearchView() {
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+        binding.searchViewResult.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if (!query.isNullOrBlank()) {
                     handleSearchResult(searchRecipes.execute(query))
@@ -46,42 +44,32 @@ class SearchResultFragment : BaseFragment<SearchResultBinding>(SearchResultBindi
     }
 
     private fun handleSearchResult(searchResult: List<Recipe>) {
-        if (searchResult.isNotEmpty()) {
-            showSearchResults(searchResult)
-        } else {
-            showEmptyState()
+        when {
+            searchResult.isNotEmpty() -> showSearchResults(searchResult)
+            else -> showEmptyState()
         }
     }
 
     private fun showSearchResults(searchResult: List<Recipe>) {
-        binding.teaImage.visibility = View.GONE
-        binding.detailsText.visibility = View.GONE
-        binding.emptyText.visibility = View.GONE
-        binding.listContainer.removeAllViews()
-
-        for (item in searchResult) {
-            val itemView = layoutInflater.inflate(R.layout.search_content, binding.listContainer, false) as ConstraintLayout
-            val itemTextView = itemView.findViewById<TextView>(R.id.kofta_text)
-            val itemImageView= itemView.findViewById<ImageView>(R.id.kofta_image)
-            val itemFavView= itemView.findViewById<ImageView>(R.id.favourite_icon)
-            val itemTime=itemView.findViewById<TextView>(R.id.cooking_time_text)
-            itemTextView.text = item.translatedRecipeName
-            itemTime.text=item.totalTimeInMins.toString()+" min"
-            Glide.with(this)
-                .load(item.imageUrl)
-                .into(itemImageView)
-            itemFavView.setOnClickListener {
-                item.isFavourite = !item.isFavourite
-                itemFavView.setBackgroundResource(if (item.isFavourite) R.drawable.favourite else R.drawable.favourite_fill)
+        binding.apply {
+            imageTea.isVisible = false
+            textDetails.isVisible = false
+            textEmptyList.isVisible = false
+            recyclerView.isVisible = true
+            binding.recyclerView.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+                adapter = recipesAdapter
             }
-
-            binding.listContainer.addView(itemView)
+            recipesAdapter.submitList(searchResult)
         }
     }
+
     private fun showEmptyState() {
-        binding.teaImage.visibility = View.VISIBLE
-        binding.detailsText.visibility = View.VISIBLE
-        binding.emptyText.visibility = View.VISIBLE
-        binding.listContainer.removeAllViews()
+        binding.apply {
+            imageTea.isVisible = true
+            textDetails.isVisible = true
+            textEmptyList.isVisible = true
+            recyclerView.isVisible = false
+        }
     }
 }
