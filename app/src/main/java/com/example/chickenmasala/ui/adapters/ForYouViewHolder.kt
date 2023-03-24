82e507857c93db782b9e332b3ee3603f981ac9e7
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-03-25 02:24:48 +0300

    make for you viewholder

--- app/src/main/java/com/example/chickenmasala/ui/adapters/ForYouViewHolder.kt
+++ app/src/main/java/com/example/chickenmasala/ui/adapters/ForYouViewHolder.kt
@@ -1,13 +1,14 @@
 package com.example.chickenmasala.ui.adapters
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chickenmasala.R
 
-class ForYouViewHolder(viewItem: View): RecyclerView.ViewHolder(viewItem) {
-    val recipeImage = viewItem.findViewById<View>(R.id.recipeImage)
-    val recipeNameTitle = viewItem.findViewById<View>(R.id.RecipeNameTitle)
-    val singleCuisine = viewItem.findViewById<View>(R.id.single_cuisine)
-    val singleTime = viewItem.findViewById<View>(R.id.single_time)
+class ForYouViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
+    val recipeImage: View = viewItem.findViewById(R.id.recipeImage)
+    val recipeNameTitle: TextView = viewItem.findViewById(R.id.RecipeNameTitle)
+    val singleCuisine: TextView = viewItem.findViewById(R.id.single_cuisine)
+    val singleTime: TextView = viewItem.findViewById(R.id.single_time)
 
 }
