062157b558af1ab66cb8db3dcdc6e0f9518763af
Taha Saad <59686887+flutterdevtaha@users.noreply.github.com>
2023-03-25 03:07:12 +0300

    merge viewholder into its adapter

--- app/src/main/java/com/example/chickenmasala/ui/adapters/ForYouRecipesAdapter.kt
+++ app/src/main/java/com/example/chickenmasala/ui/adapters/ForYouRecipesAdapter.kt
@@ -1,16 +1,16 @@
 package com.example.chickenmasala.ui.adapters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.chickenmasala.R
 import com.example.chickenmasala.entities.Recipe
-import android.widget.TextView
+import com.example.chickenmasala.databinding.HomeRecipeCardBinding
 
 class ForYouRecipesAdapter(private val forYouRecipes: List<Recipe>) :
-    RecyclerView.Adapter<ForYouViewHolder>() {
+    RecyclerView.Adapter<ForYouRecipesAdapter.ForYouViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForYouViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.home_recipe_card, parent, false)
@@ -22,21 +22,20 @@ class ForYouRecipesAdapter(private val forYouRecipes: List<Recipe>) :
 
     override fun onBindViewHolder(holder: ForYouViewHolder, position: Int) {
         val currentRecipe = forYouRecipes[position]
-        loadRecipeData(currentRecipe, holder)
-        loadRecipeImage(currentRecipe, holder)
-    }
-
-    private fun loadRecipeData(currentRecipe: Recipe, holder: ForYouViewHolder) {
-        holder.apply {
-            recipeNameTitle.text = currentRecipe.translatedRecipeName
+        holder.binding.apply {
+            Glide.with(holder.itemView.context).load(currentRecipe.imageUrl)
+                .into(recipeImage)
+            RecipeNameTitle.text = currentRecipe.translatedRecipeName
             singleCuisine.text = currentRecipe.cuisine
             singleTime.text = currentRecipe.totalTimeInMins.toString()
+
         }
     }
 
-    private fun loadRecipeImage(currentRecipe: Recipe, holder: ForYouViewHolder) {
-        Glide.with(holder.itemView.context).load(currentRecipe.imageUrl)
-            .into(holder.recipeImage as ImageView)
+
+    inner class ForYouViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
+        val binding = HomeRecipeCardBinding.bind(viewItem)
+
     }
 
 }
