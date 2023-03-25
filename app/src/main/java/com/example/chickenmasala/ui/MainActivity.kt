d6312f5e67fb89fca8cb49ecd7df80a33938dace
Abdallah <abdallahsayed3355@gmail.com>
2023-03-25 14:22:22 +0200

    update about fragment layout

--- app/src/main/java/com/example/chickenmasala/ui/MainActivity.kt
+++ app/src/main/java/com/example/chickenmasala/ui/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.chickenmasala.R
 import com.example.chickenmasala.data.DataManager
 import com.example.chickenmasala.databinding.ActivityMainBinding
+import com.example.chickenmasala.databinding.FragmentAboutBinding
+import com.example.chickenmasala.databinding.FragmentDetailsBinding
 
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    val binding by lazy { FragmentAboutBinding.inflate(layoutInflater) }
     val dataManager by lazy { DataManager(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
