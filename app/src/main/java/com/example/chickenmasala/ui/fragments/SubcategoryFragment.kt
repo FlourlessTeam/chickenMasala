c5d375440836f8917268d62a4f673e7d35d4216a
yousef <yousuf.naser2000@gmail.com>
2023-03-28 19:28:11 +0200

    convert fragment argument to bundle subcategory

--- app/src/main/java/com/example/chickenmasala/ui/fragments/SubcategoryFragment.kt
+++ app/src/main/java/com/example/chickenmasala/ui/fragments/SubcategoryFragment.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import android.view.View
 import com.example.chickenmasala.databinding.FragmentSubcategoryBinding
 import com.example.chickenmasala.entities.Cuisine
+import com.example.chickenmasala.entities.Recipe
 import com.example.chickenmasala.ui.adapters.SubcategoriesAdapter
 import com.example.chickenmasala.ui.interfaces.AppbarFragment
 
-class SubcategoryFragment(private val cuisine: Cuisine) :
+class SubcategoryFragment :
     AppbarFragment<FragmentSubcategoryBinding>(FragmentSubcategoryBinding::inflate) {
-
+    private lateinit var  cuisine: Cuisine
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpAppbarBackButton(binding.toolbarSubCategory)
+        arguments.let {
+            cuisine = it?.getParcelable(TAG)!!
+        }
         binding.toolbarSubCategory.title = cuisine.name
         val meals = cuisine.recipes
         val adapter = SubcategoriesAdapter(SubcategoriesAdapter.SubcategoryListener {
@@ -21,7 +25,15 @@ class SubcategoryFragment(private val cuisine: Cuisine) :
         binding.recyclerViewSubCategory.adapter = adapter
         adapter.submitList(meals)
     }
+companion object{
 
+   const val TAG = "SubcategoryFragment TAG"
+    fun newInstance(cuisine: Cuisine) = SubcategoryFragment().apply {
+        arguments = Bundle().apply {
+            putParcelable(TAG, cuisine)
+        }
+    }
+}
 
 
 
